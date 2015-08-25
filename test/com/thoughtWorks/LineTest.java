d509002a08b5b1bf7d66184cdcd125c18a609992
saisree <saisree@INsaisree.local>
2015-08-25 23:07:11 +0530

    failed LineTest

--- test/com/thoughtWorks/LineTest.java
+++ test/com/thoughtWorks/LineTest.java
@@ -5,8 +5,15 @@ import org.junit.Test;
 
 public class LineTest {
     @Test
-    public void goingFromZeroZeroToOneZeroHasLengthOne() {
+    public void lengthShouldBeOneWhenOneEndPointOfTheLineIsZeroZeroAndAnotherPointIsOneZero() {
         Line line = new Line(0, 0, 1, 0);
         Assert.assertEquals(1.0, line.calculateLength(), 0.001);
     }
+
+    @Test
+    public void shouldReturnFourWhenPointsOfTheLineGivenAsOneZeroAndFourZero() {
+        Line line = new Line(0, 0, 4, 0);
+        Assert.assertEquals(4.0, line.calculateLength(), 0.001);
+    }
+
 }
