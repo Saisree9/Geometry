efcc57b1c377fdaecfdf12c2f9bb21719fb48c7a
saisree <saisree@INsaisree.local>
2015-08-27 07:34:08 +0530

    "New class LineComparator  is created"

---
+++ src/com/thoughtWorks/LineComparator.java
@@ -0,0 +1,17 @@
+package com.thoughtWorks;
+
+/**
+ * Created by saisree on 8/27/15.
+ */
+public class LineComparator {
+
+    private Line line1;
+    private Line line2;
+
+    public LineComparator(Line line1, Line line2) {
+
+        this.line1 = line1;
+        this.line2 = line2;
+    }
+
+}
---
+++ test/com/thoughtWorks/LineComparatorTest.java
@@ -0,0 +1,10 @@
+package com.thoughtWorks;
+
+/**
+ * Created by saisree on 8/27/15.
+ */
+public class LineComparatorTest {
+    Line line1 = new Line(1, 1, 1, 1);
+    Line line2 = new Line(1, 1, 1, 1);
+    LineComparator lineComparator = new LineComparator(line1, line2);
+}
